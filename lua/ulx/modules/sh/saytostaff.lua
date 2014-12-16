f88e40b8b234d24ddaa5b72444f04ef48995d987
The Realist <schtherealist@hotmail.com>
2014-12-15 23:57:37 -0600

    I was an idiot and forgot to include multiple usergroups.

--- lua/ulx/modules/sh/saytostaff.lua
+++ lua/ulx/modules/sh/saytostaff.lua
@@ -1,7 +1,7 @@
 function ulx.saytostaff( calling_ply, message  )
 	
 	for k,v in pairs( player.GetAll() ) do
-		if v:IsUserGroup("superadmin") then
+		if v:IsUserGroup("superadmin") || v:IsUserGroup("admin") || v:IsUserGroup("owner") || v:IsUserGroup("wizard") || v:IsUserGroup("moderator")then
 			ULib.tsayColor( v, false, Color(255,50,0), "[STAFF]: ", team.GetColor(calling_ply:Team()), calling_ply:Nick().. ": ", Color(225,225,225), message )
 		else end
 	end
